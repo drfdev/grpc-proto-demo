118b203335af31a276c920fe5cc9851000bc438d
drfdev <drafff@gmail.com>
2020-02-16 18:40:28 +0300

    add protobuf compiler

---
+++ grpc-proto-common/src/main/java/dev/drf/grpc/demo/package-info.java
@@ -0,0 +1,11 @@
+// placeholder
+package dev.drf.grpc.demo;
+
+/*
+// Generated:
+import dev.drf.grpc.demo.Service;
+import dev.drf.grpc.demo.dto.RequestOuterClass.Request;
+import dev.drf.grpc.demo.dto.ResponseOuterClass.Response;
+import dev.drf.grpc.demo.dto.ResponseOuterClass.Event;
+import dev.drf.grpc.demo.dto.ResponseOuterClass.Status;
+ */
